f9b7e07c829e6e7dc7ec7ea500fd03102cbd3317
Anna <anyalar04@gmail.com>
2024-10-18 23:41:17 +0300

    refactor: use js obj to display header items

---
+++ src/js/app.js
@@ -0,0 +1,109 @@
+const menuItems = [
+  {
+    title: 'Why Spring',
+    submenu: [
+      { title: 'Overview', href: '/why-spring' },
+      { title: 'Microservices', href: '/microservices' },
+      { title: 'Reactive', href: '/reactive' },
+      { title: 'Event Driven', href: '/event-driven' },
+      { title: 'Cloud', href: '/cloud' },
+      { title: 'Web Applications', href: '/web-applications' },
+      { title: 'Serverless', href: '/serverless' },
+      { title: 'Batch', href: '/batch' },
+    ],
+  },
+  {
+    title: 'Learn',
+    submenu: [
+      { title: 'Overview', href: '/learn' },
+      { title: 'Quickstart', href: '/quickstart' },
+      { title: 'Guides', href: '/guides' },
+      { title: 'Blog', href: '/blog' },
+    ],
+  },
+  {
+    title: 'Projects',
+    submenu: [
+      { title: 'Overview', href: '/projects' },
+      { title: 'Spring Boot', href: '/projects/spring-boot' },
+      { title: 'Spring Framework', href: '/projects/spring-framework' },
+      { title: 'Spring Cloud', href: '/projects/spring-cloud' },
+      {
+        title: 'Spring Cloud Data Flow',
+        href: '/projects/spring-cloud-dataflow',
+      },
+      { title: 'Spring Data', href: '/projects/spring-data' },
+      { title: 'Spring Integration', href: '/projects/spring-integration' },
+      { title: 'Spring Batch', href: '/projects/spring-batch' },
+      { title: 'Spring Security', href: '/projects/spring-security' },
+      { title: 'Spring AI', href: '/projects/spring-ai' },
+      { title: 'View all projects', href: '/projects' },
+      { title: 'DEVELOPMENT TOOLS', href: '#' },
+      { title: 'Spring Tools 4', href: '/tools' },
+      { title: 'Spring Initializer', href: 'https://start.spring.io/' },
+    ],
+  },
+  {
+    title: 'Academy',
+    submenu: [
+      { title: 'Courses', href: 'https://spring.academy/courses' },
+      { title: 'Get Certified', href: 'https://spring.academy/learning-path' },
+    ],
+  },
+  {
+    title: 'Solutions',
+    submenu: [
+      { title: 'Overview', href: '/solutions' },
+      { title: 'Tanzu Spring', href: '/support' },
+      { title: 'Spring Consulting', href: '/consulting' },
+      {
+        title: 'Spring Academy For Teams',
+        href: 'https://spring.academy/teams',
+      },
+      { title: 'Security Advisories', href: '/security' },
+    ],
+  },
+  {
+    title: 'Community',
+    submenu: [
+      { title: 'Overview', href: '/community' },
+      { title: 'Events', href: '/events' },
+      { title: 'Authors', href: '/authors' },
+    ],
+  },
+];
+
+function generateMenu(menuItems) {
+  const menu = document.querySelector('#menu');
+
+  menuItems.forEach((item) => {
+    const menuItem = document.createElement('li');
+    menuItem.classList.add('header__menu-item');
+
+    const link = document.createElement('a');
+    link.href = '#';
+    link.textContent = item.title;
+    menuItem.appendChild(link);
+
+    if (item.submenu) {
+      const submenu = document.createElement('ul');
+      submenu.classList.add('header__submenu');
+
+      item.submenu.forEach((subitem) => {
+        const submenuItem = document.createElement('li');
+        const subLink = document.createElement('a');
+        subLink.classList.add('header__submenu-item');
+        subLink.href = subitem.href;
+        subLink.textContent = subitem.title;
+        submenuItem.appendChild(subLink);
+        submenu.appendChild(submenuItem);
+      });
+
+      menuItem.appendChild(submenu);
+    }
+
+    menu.appendChild(menuItem);
+  });
+}
+
+generateMenu(menuItems);
