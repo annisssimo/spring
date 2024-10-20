c358aa911510db5326a8fec38e770ad576ddc06d
Anna <anyalar04@gmail.com>
2024-10-20 18:56:08 +0300

    feat: implement projects search

--- src/js/components/projects.js
+++ src/js/components/projects.js
@@ -1,34 +1,55 @@
 import { projects } from '../data/projects.js';
 
-export function generateProjects() {
-  const projectsContainer = document.querySelector('.projects__container');
+const searchInput = document.getElementById('projects-search');
+const projectsContainer = document.querySelector('.projects__container');
+const noResults = document.getElementById('no-results');
 
-  projects.forEach((proj) => {
-    const project = document.createElement('div');
-    project.classList.add('projects-card');
+export function generateProjects(filteredProjects = projects) {
+  projectsContainer.innerHTML = '';
+  noResults.style.display = 'none';
 
-    const imageDiv = document.createElement('div');
-    imageDiv.classList.add('project-card__image');
-    const image = document.createElement('img');
-    image.setAttribute('src', proj.image);
-    image.setAttribute('alt', proj.title);
+  if (filteredProjects.length === 0) {
+    noResults.style.display = 'block';
+  } else {
+    filteredProjects.forEach((proj) => {
+      const project = document.createElement('div');
+      project.classList.add('projects-card');
 
-    const projectsInfo = document.createElement('div');
-    projectsInfo.classList.add('projects__info');
+      const imageDiv = document.createElement('div');
+      imageDiv.classList.add('project-card__image');
+      const image = document.createElement('img');
+      image.setAttribute('src', proj.image);
+      image.setAttribute('alt', proj.title);
 
-    const title = document.createElement('h3');
-    title.classList.add('projects__title');
-    title.textContent = proj.title;
+      const projectsInfo = document.createElement('div');
+      projectsInfo.classList.add('projects__info');
 
-    const description = document.createElement('p');
-    description.classList.add('projects__description');
-    description.textContent = proj.description;
+      const title = document.createElement('h3');
+      title.classList.add('projects__title');
+      title.textContent = proj.title;
 
-    projectsContainer.append(project);
-    project.append(imageDiv);
-    imageDiv.append(image);
-    project.append(projectsInfo);
-    projectsInfo.append(title);
-    projectsInfo.append(description);
-  });
+      const description = document.createElement('p');
+      description.classList.add('projects__description');
+      description.textContent = proj.description;
+
+      projectsContainer.append(project);
+      project.append(imageDiv);
+      imageDiv.append(image);
+      project.append(projectsInfo);
+      projectsInfo.append(title);
+      projectsInfo.append(description);
+    });
+  }
 }
+
+function filterProjects() {
+  const query = searchInput.value.toLowerCase();
+  const filteredProjects = projects.filter(
+    (proj) =>
+      proj.title.toLowerCase().includes(query) ||
+      proj.description.toLowerCase().includes(query)
+  );
+  generateProjects(filteredProjects);
+}
+
+searchInput.addEventListener('input', filterProjects);
